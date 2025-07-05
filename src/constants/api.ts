58f2a5bc6380fa257384909c1f405e69fdec110c
guruGit008 <gurutirangaaerospace@gmail.com>
2025-07-05 13:03:04 +0530

    changed url link

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,8 +1,8 @@
-// export const APIURL = 'http://localhost:8080'; 
+// // export const APIURL = 'http://localhost:8080'; 
 
 
-// export const APIURL = 'https://hrtool-backend.onrender.com'; 
+export const APIURL = 'https://hrtool-backend.onrender.com'; 
 
-// export const APIURL = process.env.NEXT_PUBLIC_API_URL!;
+// // export const APIURL = process.env.NEXT_PUBLIC_API_URL!;
 
-export const APIURL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080';
+// export const APIURL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080';
